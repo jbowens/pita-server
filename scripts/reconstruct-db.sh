0693d54353df80f338b7a36bf17ceed808b6f3b7
Jackson Owens <jackson_owens@brown.edu>
2014-02-10 10:52:02 -0500

    more structure to sql directory

--- scripts/reconstruct-db.sh
+++ scripts/reconstruct-db.sh
@@ -2,6 +2,5 @@
 SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
 SQL_DIR=$SCRIPT_DIR/../sql
 cat "$SQL_DIR/drop-everything.sql" \
-    "$SQL_DIR/accounts.sql" \
-    "$SQL_DIR/pitas.sql" \
+    $SQL_DIR/tables/*.sql \
     | psql --dbname=mylittlepita --file=-

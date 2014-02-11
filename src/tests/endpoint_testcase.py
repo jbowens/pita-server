4bc3b8450076d09f8b160625237d2300d603485b
Jackson Owens <jackson_owens@brown.edu>
2014-02-11 15:43:46 -0500

    added ulility testing method for creating random accountts

--- src/tests/endpoint_testcase.py
+++ src/tests/endpoint_testcase.py
@@ -1,11 +1,32 @@
-import unittest, sys, os
+import unittest, sys, os, random
 
 sys.path.append(os.path.dirname(__file__) + '/..')
+
+def random_word(length):
+       return ''.join(random.choice(string.lowercase) for i in range(length))
+def random_phone():
+       return ''.join(random.choice(string.digits) for i in range(11))
 
 from mylittlepita import app
 
 class EndpointTestCase(unittest.TestCase):
-    
+ 
+    def rand_account(self):
+        """
+        Creates a random account for testing.
+        """
+        rv = self.app.post('/accounts/new', data = {
+            'name': 'Test Account',
+            'email': random_word(8) + '@' + random_word(10) + '.' + random_word(3)
+        })
+        return json.loads(rv.data)
+
+    def n_rand_accounts(self, n):
+        """
+        Creates a list of n random accounts.
+        """
+        return [self.random_account() for x in range(n)]
+   
     def setUp(self):
         self.app = app.test_client()
 
